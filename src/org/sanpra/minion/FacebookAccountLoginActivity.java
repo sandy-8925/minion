04566bf3343a47adf47f0314299fd0e632901306
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-08-14 17:07:46 -0700

    FacebookAccountLoginActivity: Remove unused imports

--- src/org/sanpra/minion/FacebookAccountLoginActivity.java
+++ src/org/sanpra/minion/FacebookAccountLoginActivity.java
@@ -23,14 +23,9 @@ package org.sanpra.minion;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import com.facebook.Request;
-import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
-import com.facebook.model.GraphUser;
-import com.facebook.widget.ProfilePictureView;
 
 public final class FacebookAccountLoginActivity extends Activity
 {
