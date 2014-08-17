6e0ae63b40c40cf6c27a90c7fae41d83579467f3
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-08-16 19:36:09 -0700

    Create new class FacebookAccount to handle Facebook account

---
+++ src/org/sanpra/minion/FacebookAccount.java
@@ -0,0 +1,18 @@
+package org.sanpra.minion;
+
+import com.facebook.Session;
+
+
+public final class FacebookAccount {
+    private static Session session;
+
+    private FacebookAccount() {}
+
+    public static Session getSession() {
+        return session;
+    }
+
+    static void setSession(Session newSession) {
+        session = newSession;
+    }
+}
