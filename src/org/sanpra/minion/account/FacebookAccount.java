ac1810b7ac5292e844eff46e13a3ca6833d48cbc
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-08-30 23:32:07 -0700

    FacebookAccount: Make method getSession private

--- src/org/sanpra/minion/account/FacebookAccount.java
+++ src/org/sanpra/minion/account/FacebookAccount.java
@@ -34,7 +34,7 @@ public final class FacebookAccount {
 
     private FacebookAccount() {}
 
-    public static Session getSession() {
+    private static Session getSession() {
         return session;
     }
 
