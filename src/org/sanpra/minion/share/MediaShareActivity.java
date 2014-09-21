2f334e1ec97a8aa04b84ea46e374ff20b67aa8e5
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-09-20 20:35:42 -0700

    MediaShareActivity: Create new method buildUriDisplayString - convenience method for displaying Uri passed to the acitvity

--- src/org/sanpra/minion/share/MediaShareActivity.java
+++ src/org/sanpra/minion/share/MediaShareActivity.java
@@ -67,6 +67,21 @@ public final class MediaShareActivity extends FragmentActivity {
         }
     }
 
+    /**
+     * Accepts a collection of Uri objects, and builds a displayable string, with string representation of each Uri on it's own line
+     * @param uriList Input collection of Uri objects
+     * @return User displayable string, with string representation of each Uri on it's own line
+     */
+    private static String buildUriDisplayString(Collection<Uri> uriList) {
+        StringBuilder uriListString = new StringBuilder();
+
+        for(Uri uri : uriList) {
+            uriListString.append(uri.toString()).append('\n');
+        }
+
+        return uriListString.toString();
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
