1c99beed7c511675432eaba5e030f9913ca77b31
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-12-20 23:43:27 -0800

    Add new class NotificationUtils. Currently contains constant data - notification IDs.

---
+++ src/org/sanpra/minion/utils/NotificationUtils.java
@@ -0,0 +1,7 @@
+package org.sanpra.minion.utils;
+
+public final class NotificationUtils {
+    public static final int UPLOAD_ERROR_NOTIFICATION_ID = 1;
+    public static final int UPLOAD_SUCCESS_NOTIFICATION_ID = 2;
+    public static final int FILE_NOT_FOUND_NOTIFICATION_ID = 3;
+}
